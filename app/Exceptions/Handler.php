9011d94e0c90399fe4b855e71fbcbeeed88b7dd3
Sharwan Kami <sharwankami@gmail.com>
2022-08-24 21:31:23 +0200

    exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +38,22 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, Request $request){
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message'=>'Not Found'
+                ], 404);
+            }
+        });
+
+        $this->renderable(function (\RuntimeException $e, Request $request){
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message'=>'Page Expired'
+                ], 419);
+            }
         });
     }
 }
